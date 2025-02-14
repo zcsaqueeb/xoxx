4e58cc88fcab3339f83bf52ba556ad4db0d5d651
I AM CONFFIN <saqueebbegumpure@gmail.com>
2025-02-14 18:24:58 +0530

    Add files via upload

---
+++ core/header.js
@@ -0,0 +1,18 @@
+const headers = {
+  Accept: "*/*",
+  "Accept-Encoding": "gzip, deflate, br",
+  "Accept-Language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
+  "Content-Type": "application/json",
+  Origin: "https://miniapp.xname.app",
+  referer: "https://miniapp.xname.app/",
+  "Sec-Ch-Ua": '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
+  "Sec-Ch-Ua-Mobile": "?0",
+  "Sec-Fetch-Dest": "empty",
+  "Sec-Fetch-Mode": "cors",
+  "Sec-Fetch-Site": "same-origin",
+  "Cache-Control": "no-cache",
+  "user-device": "Edge/131.0.0.0#Windows/10",
+  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+};
+
+module.exports = headers;
